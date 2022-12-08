a17f3e4d0725f996119de48c7bface16b7e0646e
Diaszano <lucasdiiassantos@gmail.com>
2022-12-08 19:08:49 +0000

    add prisma

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 
 @Injectable()
 export class PokemonService {
+  constructor(private readonly __prisma: PrismaService) {}
+
   create(createPokemonDto: CreatePokemonDto) {
     return 'This action adds a new pokemon';
   }
 
   findAll() {
-    return `This action returns all pokemon`;
+    return this.__prisma.pokemons.findMany();
   }
 
   findOne(id: number) {
